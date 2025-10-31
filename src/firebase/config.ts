10d8f97af8dd32e9ded21b969be8d63c10e5e21d
Felipe Saito <fesaito.ia@gmail.com>
2025-10-31 14:04:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3449857100-b1dcc",
+  "appId": "1:344069549947:web:85cc12b3c4787acb2f17de",
+  "apiKey": "AIzaSyDzAm41MfwO4ohyGVe9DX3bBr3siECwBi0",
+  "authDomain": "studio-3449857100-b1dcc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "344069549947"
+};
