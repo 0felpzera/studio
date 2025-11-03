3994c60fedfe1b473ac91c4e2084ceb2cba6041f
Arthur Alex <artvaldivia2013@gmail.com>
2025-11-03 13:38:14 +0000

    instagram logo, threads, x e facebook nao estao aparecendol.

--- src/components/ui/logo-cloud-3.tsx
+++ src/components/ui/logo-cloud-3.tsx
@@ -26,7 +26,7 @@ export function LogoCloud({ className, logos, ...props }: LogoCloudProps) {
         {logos.map((logo) => (
           <img
             alt={logo.alt}
-            className="pointer-events-none h-4 select-none md:h-5 dark:brightness-0 dark:invert"
+            className="pointer-events-none h-4 select-none md:h-5 dark:filter dark:invert"
             height={logo.height || "auto"}
             key={`logo-${logo.alt}`}
             loading="lazy"
