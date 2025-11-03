966fe994f456a44d77bf3784c70df07b2b48ee00
Arthur Alex <artvaldivia2013@gmail.com>
2025-11-03 14:25:20 +0000

    tire a cor lilas do bg das sections, deixe branco mesmo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -145,7 +145,7 @@ export default function LandingPage() {
         icons={demoIcons}
       />
       
-      <div className="bg-[#fff3ff] rounded-t-[50px]">
+      <div className="bg-background rounded-t-[50px]">
         <Timeline data={timelineData} />
 
          <motion.section 
